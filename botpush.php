0e4f29e41fc7d962da28e14e5033d560bf2e4b25
somsakkyy <somsakkyy@gmail.com>
2018-06-06 10:27:57 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'fGQPBQRT9vIBi/ZnjM5fYiyAhAAM1mB4waFwnl4OEPM5rpLqnuCq0Kd6GyG10YcwKuoPbrSKUmeGnEhmSeBi/P/kkdt2d7iiTOkXmaX7xuy70+tjLHLSaaCrVpXtR3Dl63T33LPnt42f4VEIswYLKwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '0806b2fdb2e375097093f366a8c1b96d';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U8c70a677bf133bf1675136a943222105';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
